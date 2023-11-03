2f77542ec94dd0064e98e25a2df88bba848ed472
Henry Ifebunandu <henryifebunandu@gmail.com>
2023-11-03 01:17:42 +0100

    Initial Commit

---
+++ war challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war challenge
+//
+//  Created by Henry Ifebunandu on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
