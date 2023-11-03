c4354bdb942aac82a836745fc402f138ee454935
Henry Ifebunandu <henryifebunandu@gmail.com>
2023-11-03 12:09:17 +0100

    complete code war challenge with swift

--- war challenge/ContentView.swift
+++ war challenge/ContentView.swift
@@ -8,19 +8,105 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card5"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Image("background")
+            
+            VStack {
+                
+                Spacer()
+                
+                Image("logo")
+                
+                Spacer()
+                
+                HStack{
+                    
+                    Spacer()
+                    
+                    Image(playerCard)
+                    
+                    Spacer()
+                    
+                    Image(cpuCard)
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+                
+               
+                
+                Button {
+                    let playerRandom = Int.random(in: 2...14)
+                    let cpuRandom = Int.random(in: 2...14)
+                    
+                   playerCard = "card" + String(playerRandom)
+                    cpuCard = "card" + String(cpuRandom)
+                    
+                    if (playerRandom > cpuRandom) {
+                        playerScore += 1
+                    } else if (cpuRandom > playerRandom){
+                        cpuScore += 1
+                    }
+                    
+                    
+                } label: {
+                    Image("dealbutton")
+                }
+
+                
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    // player
+                    VStack(spacing: 10.0){
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                        
+                        
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    
+                    Spacer()
+                    
+                    // cpu
+                    VStack(spacing: 10.0){
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                        
+                        
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            
+                            .foregroundColor(Color.white)
+                    }
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group{
+            ContentView()
+            ContentView()
+                .previewDevice("iPhone 8 Plus")
+        }
     }
 }
